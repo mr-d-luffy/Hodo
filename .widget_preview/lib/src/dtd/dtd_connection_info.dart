e29099bb5f1c52831b926fe6f674f693d764e5e9
mr-d-luffy <zoroxpro@proton.me>
2026-08-16 15:38:31 +0530

    priview removed

--- .widget_preview/lib/src/dtd/dtd_connection_info.dart
+++
@@ -1,8 +0,0 @@
-// ignore_for_file: implementation_imports
-
-const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:51028/WX2SwQ6gTYY=';
-const String kWidgetPreviewService =
-    'widget-preview-9213bd14-da56-46f3-b13b-de7e728b4ee6';
-const String kWidgetPreviewScaffoldStream =
-    'WidgetPreviewScaffold-9213bd14-da56-46f3-b13b-de7e728b4ee6';
-const String kProjectRootPath = r'/Users/mr.silicon/DartProjects/projects/hodo';
